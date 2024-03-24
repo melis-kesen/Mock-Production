010cddc46be5e0b0b277d6116aee1adb7e3be713
Melis <meliskesen@gmail.com>
2024-03-24 17:58:25 +0300

    💸

--- Backend/src/product.module.ts
+++ Backend/src/product.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { Product } from './entities/Product.entity';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db', // PostgreSQL sunucu adresi
-      port: 5432, // PostgreSQL port numarası
-      username: 'postgres', // PostgreSQL kullanıcı adı
-      password: 'postgres', // PostgreSQL şifre
-      database: 'postgres', // PostgreSQL veritabanı adı
+      host: process.env.DB_HOST, // PostgreSQL sunucu adresi
+      port: parseInt(process.env.DB_PORT), // PostgreSQL port numarası
+      username: process.env.DB_USERNAME, // PostgreSQL kullanıcı adı
+      password: process.env.DB_PASSWORD, // PostgreSQL şifre
+      database: process.env.DB_DATABASE, // PostgreSQL veritabanı adı
       entities: [__dirname + '/**/*.entity{.ts,.js}'], // TypeORM varlık dosyalarının yolu
       autoLoadEntities: true,
       synchronize: true,
