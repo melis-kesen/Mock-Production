79642133eec7c7237575aaef42efbaa051154e9a
Melis <meliskesen@gmail.com>
2024-03-27 19:53:10 +0300

    📝

---
+++ Backend/src/ormconfig.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost', // PostgreSQL sunucu adresi
+  port: 5432, // PostgreSQL port numarası
+  username: 'postgres', // PostgreSQL kullanıcı adı
+  password: 'rabarba', // PostgreSQL şifre
+  database: 'Test', // PostgreSQL veritabanı adı
+  entities: [__dirname + '/**/*.entity{.ts,.js}'], // TypeORM varlık dosyalarının yolu
+  //synchronize: true, // Otomatik tablo senkronizasyonunu etkinleştir
+};
+
+export default config;
